bdbff48a82be5aa93876f88cdae7b47599b6998b
zxy <358996085@qq.com>
2021-03-09 20:46:00 +0800

    router抽离及手机aside切换时关闭

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
 
 <script lang="ts">
 import { provide, ref } from "vue";
+import { router } from "./router";
 
 export default {
   name: "App",
@@ -11,6 +12,11 @@ export default {
     const width = document.documentElement.clientWidth;
     const asideVisible = ref(width > 500 ? true : false);
     provide("asideVisible", asideVisible);
+    router.afterEach((to, from) => {
+      if (width <= 500) {
+        asideVisible.value = false;
+      }
+    });
   },
 };
 </script>
---
+++ src/router.ts
@@ -0,0 +1,17 @@
+import { createWebHashHistory, createRouter } from 'vue-router'
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
+import Switch from './components/Switch.vue'
+
+const history = createWebHashHistory()
+export const router = createRouter({
+    history,
+    routes: [
+        { path: '/', component: Home },
+        {
+            path: '/doc', component: Doc, children: [
+                { path: 'switch', component: Switch }
+            ]
+        }
+    ]
+})
