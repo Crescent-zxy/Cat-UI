64fc45e35646d5a4125f6eb468676a9f9d5afe76
zxy <358996085@qq.com>
2021-03-09 16:02:13 +0800

    Home样式完善

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,44 @@
 <template>
-    <div>
-        <Topnav />
-        <div class="banner">
-            <h1>CatUI</h1>
-            <h2>一个基于 Vue3 的 UI 框架</h2>
-            <p class="actions">
-                <a href="">GitHub</a>
-                <a href="">开始</a>
-            </p>
-        </div>
+  <div>
+    <Topnav />
+    <div class="banner">
+      <h1>CatUI</h1>
+      <h2>一个基于 Vue3 的 UI 框架</h2>
+      <p class="actions">
+        <a href="">GitHub</a>
+        <router-link to="/doc">开始</router-link>
+      </p>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
-import Topnav from '../components/Topnav.vue'
+import Topnav from "../components/Topnav.vue";
 export default {
-    components: { Topnav }
-}
+  components: { Topnav },
+};
 </script>
+
+<style lang="scss" scoped>
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions {
+    padding: 8px 0;
+    a {
+      margin: 0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
+</style> 
