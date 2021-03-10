fcdb5435247a59a2929db0a5c45b2d9baa7f1f02
zxy <358996085@qq.com>
2021-03-10 17:04:40 +0800

    Button添加theme属性

--- src/components/ButtonPage.vue
+++ src/components/ButtonPage.vue
@@ -1,7 +1,12 @@
 <template>
   <div>Button 示例</div>
   <h1>示例1</h1>
-  <Button></Button>
+  <p>
+    <Button>你好</Button>
+    <Button theme="button">你好</Button>
+    <Button theme="link">你好</Button>
+    <Button theme="text">你好</Button>
+  </p>
 </template>
 
 <script lang="ts">
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,19 @@
 <template>
-  <button>
+  <button :class="`theme-${theme}`">
     <slot />
   </button>
 </template>
+
+<script lang="ts">
+export default {
+  props: {
+    theme: {
+      type: String,
+      default: "button",
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
