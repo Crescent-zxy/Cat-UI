2a0cb056bca1649fab2d41fa7e5a264df6be2454
zxy <358996085@qq.com>
2021-03-10 19:31:29 +0800

    Button添加theme样式及CSS规范

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -49,5 +49,23 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+  &.cat-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,
+    &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+  &.cat-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
 }
 </style>
