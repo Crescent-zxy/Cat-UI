651515c2fe98bb7cda21a91a2d7c9c5bd37a445d
zxy <358996085@qq.com>
2021-03-10 11:20:32 +0800

    Switch添加value属性和input事件

--- src/components/SwitchPage.vue
+++ src/components/SwitchPage.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    <Switch />
+    <Switch :value="y" @input="y = $event"/>
   </div>
 </template>
 
 <script lang="ts">
+import { ref } from 'vue';
 import Switch from "../lib/Switch.vue";
 export default {
   components: { Switch },
+  setup(){
+      const y = ref(false)
+      return { y }
+  }
 };
 </script>
