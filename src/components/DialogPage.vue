34e2c83b5d949b1b349ad53503179444702d930c
zxy <358996085@qq.com>
2021-03-11 12:32:02 +0800

    Dialog添加visible属性

--- src/components/DialogPage.vue
+++ src/components/DialogPage.vue
@@ -1,13 +1,24 @@
 <template>
   <h2>Dialog 示例</h2>
+  <hr />
   <p>
-    <Dialog />
+    <Button @click="toggle">toggle</Button>
+    <Dialog :visible="bool"></Dialog>
   </p>
 </template>
 
 <script lang="ts">
+import { ref } from "vue";
+import Button from "../lib/Button.vue";
 import Dialog from "../lib/Dialog.vue";
 export default {
-  components: { Dialog },
+  components: { Dialog, Button },
+  setup() {
+    const bool = ref(false);
+    const toggle = () => {
+      bool.value = !bool.value;
+    };
+    return { bool, toggle };
+  },
 };
 </script>
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,26 +1,34 @@
 <template>
-  <div class="cat-dialog-overlay"></div>
-  <div class="cat-dialog-wrapper">
-    <div class="cat-dialog">
-      <header>
-        标题
-        <div class="cat-dialog-close"></div>
-      </header>
-      <main>
-        <p>第一行字</p>
-        <p>第二行字</p>
-      </main>
-      <footer>
-        <Button level="main">OK</Button>
-        <Button>Cancel</Button>
-      </footer>
+  <template v-if="visible">
+    <div class="cat-dialog-overlay"></div>
+    <div class="cat-dialog-wrapper">
+      <div class="cat-dialog">
+        <header>
+          标题
+          <div class="cat-dialog-close"></div>
+        </header>
+        <main>
+          <p>第一行字</p>
+          <p>第二行字</p>
+        </main>
+        <footer>
+          <Button level="main">OK</Button>
+          <Button>Cancel</Button>
+        </footer>
+      </div>
     </div>
-  </div>
+  </template>
 </template>
 
 <script lang="ts">
 import Button from "./Button.vue";
 export default {
+  props: {
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+  },
   components: { Button },
 };
 </script>
