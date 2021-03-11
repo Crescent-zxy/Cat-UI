b3434543ebfcdb0e1c35f58555412c64bc19286a
zxy <358996085@qq.com>
2021-03-11 16:04:12 +0800

    Tabs添加CSS

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,6 +1,14 @@
 <template>
-  <div v-for="(t, index) in titles" :key="index">{{ t }}</div>
-  <component v-for="(c, index) in defaults" :is="c" :key="index" />
+  <div class="cat-tabs">
+    <div class="cat-tabs-nav">
+      <div class="cat-tabs-nav-item" v-for="(t, index) in titles" :key="index">
+        {{ t }}
+      </div>
+    </div>
+    <div class="cat-tabs-content">
+      <component v-for="(c, index) in defaults" :is="c" :key="index" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -18,3 +26,30 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.cat-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
