9ebd2fc3dca18f3a35d1da0ffde5f4b8609564fe
zxy <358996085@qq.com>
2021-03-13 17:55:30 +0800

    DialogDemos

--- src/components/DialogPage.vue
+++ src/components/DialogPage.vue
@@ -1,61 +1,17 @@
 <template>
-  <h2>Dialog 示例</h2>
-  <hr />
-  <p>
-    <Button @click="toggle">toggle</Button>
-    <Dialog
-      v-model:visible="bool"
-      :closeOnClickOverlay="false"
-      :ok="f1"
-      :cancel="f2"
-    >
-      <template v-slot:title>
-        <h3>标题</h3>
-      </template>
-      <template v-slot:content>
-        <p>第一行</p>
-        <p>第二行</p>
-      </template>
-    </Dialog>
-  </p>
-  <h3>一句话打开 Dialog 示例</h3>
-  <p>
-    <Button @click="showDialog">函数打开 Dialog</Button>
-  </p>
+  <h1>Dialog 示例</h1>
+  <Demo :component="DialogDemo1" />
+  <Demo :component="DialogDemo2" />
 </template>
 
 <script lang="ts">
-import { ref } from "vue";
-import Button from "../lib/Button.vue";
-import Dialog from "../lib/Dialog.vue";
-import { openDialog } from "../lib/openDialog";
+import Demo from "./Demo.vue";
+import DialogDemo1 from "./DialogDemo1.vue";
+import DialogDemo2 from "./DialogDemo2.vue";
 export default {
-  components: { Dialog, Button },
+  components: { Demo },
   setup() {
-    const bool = ref(false);
-    const toggle = () => {
-      bool.value = !bool.value;
-    };
-    const f1 = () => {
-      console.log("ok");
-      return true;
-    };
-    const f2 = () => {
-      return true;
-    };
-    const showDialog = () => {
-      openDialog({
-        title: "提示",
-        content: "你好",
-        ok() {
-          console.log("ok");
-        },
-        cancel() {
-          console.log("cancel");
-        },
-      });
-    };
-    return { bool, toggle, f1, f2, showDialog };
+    return { DialogDemo1, DialogDemo2 };
   },
 };
 </script>
