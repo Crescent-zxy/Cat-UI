b26cec3d63667f654f9e5dc71c9ed48409bdfc14
zxy <358996085@qq.com>
2021-03-13 17:02:32 +0800

    Demo示例抽离

--- src/components/SwitchPage.vue
+++ src/components/SwitchPage.vue
@@ -1,17 +1,82 @@
 <template>
   <div>
-    <Switch v-model:value="bool" />
+    <h1>Switch 组件示例</h1>
+    <div class="demo">
+      <h2>常规用法</h2>
+      <div class="demo-component">
+        <SwitchDemo1 />
+      </div>
+      <div class="demo-actions">
+        <Button>查看代码</Button>
+      </div>
+      <div class="demo-code">
+        <pre>&lt;Switch v-model:value="bool" /&gt;</pre>
+      </div>
+    </div>
+    <div class="demo">
+      <h2>支持 disabled</h2>
+      <div class="demo-component">
+        <SwitchDemo2 />
+      </div>
+      <div class="demo-actions">
+        <Button>查看代码</Button>
+      </div>
+      <div class="demo-code">
+        <pre>&lt;Switch v-model:value="bool" disabled /&gt;</pre>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { ref } from "vue";
 import Switch from "../lib/Switch.vue";
+import Button from "../lib/Button.vue";
+import SwitchDemo1 from "../components/SwitchDemo1.vue";
+import SwitchDemo2 from "../components/SwitchDemo2.vue";
+import { ref } from "vue";
+
 export default {
-  components: { Switch },
+  components: {
+    Switch,
+    Button,
+    SwitchDemo1,
+    SwitchDemo2,
+  },
   setup() {
     const bool = ref(false);
     return { bool };
+    return {
+      bool,
+    };
   },
 };
 </script>
+
+<style lang="scss" scoped>
+$border-color: #d9d9d9;
+.demo {
+  border: 1px solid $border-color;
+  margin: 16px 0 32px;
+  > h2 {
+    font-size: 20px;
+    padding: 8px 16px;
+    border-bottom: 1px solid $border-color;
+  }
+  &-component {
+    padding: 16px;
+  }
+  &-actions {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+  }
+  &-code {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+    > pre {
+      line-height: 1.1;
+      font-family: Consolas, "Courier New", Courier, monospace;
+      margin: 0;
+    }
+  }
+}
+</style>
